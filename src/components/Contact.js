8265b7e718892f43d9ea9c27a90fab422888bdca
Austin Small <austinsmall@macbook-pro-2.lan>
2022-11-15 23:02:24 -0500

    commit

---
+++ src/components/Contact.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function Contact() {
+  return (
+    <section id="contact" className="relative">
+      <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
+          <div className="bg-gray-900 relative flex flex-wrap py-6 rounded shadow-md">
+            <div className="lg:w-1/2 px-6 mt-4 lg:mt-0">
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs">
+                EMAIL
+              </h2>
+              <a className="text-indigo-400 leading-relaxed">
+                AustintSmall1r@gmail.com
+              </a>
+              <h2 className="title-font font-semibold text-white tracking-widest text-xs mt-4">
+                PHONE
+              </h2>
+              <p className="leading-relaxed">(919)717-9999</p>
+            </div>
+          </div>
+        </div>
+        </section>
+  )
+}
---
+++ src/data.js
@@ -0,0 +1,23 @@
+export const projects = [
+    {
+      title: "Genre-Buddy",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./Genrebuddy.png",
+      link: "https://maxbransontaylor.github.io/genre-buddy/",
+    },
+    {
+      title: "Holiday Hiway",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./holiday-hiway.png",
+      link: "https://holiday-hiway.herokuapp.com/",
+    },
+    {
+      title: "NIM-Portal",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      image: "./Nim.png",
+      link: "http://nim-portal.herokuapp.com/",
+    },
+  ];
